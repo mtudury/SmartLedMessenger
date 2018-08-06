e85076903788e11d79154407f083bcee190e25a7
Matthieu Tudury <matthieu.tudury@gmail.com>
2018-08-07 01:28:38 +0200

    customise : static messages

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SmartLedMessenger
 Source code for Smart Led Messenger
+
+## Install
+
+config arduino sketch pour le projet
+
+Dans menu preferences : additional boards manager urls : http://arduino.esp8266.com/staging/package_esp8266com_index.json
+
+
+wifimanager dans arduino sketch : menu : include libraries ... :
+- WifiManager by tzapu (testé avec la 0.12.0)
+- adafruit gfx library by adafruit (testé avec la 1.2.7)
+- Time by Michael Margolis 1.5.0
+
+aller chercher les sources : https://github.com/markruys/arduino-Max72xxPanel (Commits on Jul 28, 2018 : 9a14fba587579fb7257396e55535851aaf9ece9c : testé ok)
+et les mettre : dans le dossier librairies de arduino sketch : 
+- C:\Users\matth\Documents\Arduino\libraries\Adafruit_GFX_Library
+
+
+Verify / Compile
--- SmartLedMessengerV1.ino
+++ SmartLedMessengerV1.ino
@@ -12,11 +12,13 @@ See more at http://dsbird.org.uk
 */
 
 //################# LIBRARIES ##########################
+#include <TimeLib.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
 //#include <WiFiClient.h>
-#include <WiFiManager.h>     // https://github.com/tzapu/WiFiManager
+//#include <WiFiManager.h>     // https://github.com/tzapu/WiFiManager
 #include <DNSServer.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
@@ -28,6 +30,21 @@ int numberOfHorizontalDisplays = 4;
 int numberOfVerticalDisplays   = 1;
 char time_value[20];
 String message, webpage;
+bool msgstatic = false;
+
+// WIFI
+const char* ssid = "REPLACE_WITH_YOUR_SSID"; //Enter SSID
+const char* password = "REPLACE_WITH_YOUR_WIFI_PWD"; //Enter Password
+
+// NTP
+IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
+// IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov
+// IPAddress timeServer(132, 163, 4, 103); // time-c.timefreq.bldrdoc.gov
+// or your local NTP server
+int timeZone = 1; 
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
 
 //################# DISPLAY CONNECTIONS ################
 // LED Matrix Pin -> ESP8266 Pin
@@ -38,7 +55,7 @@ String message, webpage;
 // CLK            -> D5  (Same Pin for WEMOS)
 
 //################ PROGRAM SETTINGS ####################
-String version = "v2.0";       // Version of this program
+String version = "v2.1";       // Version of this program
 ESP8266WebServer server(80); // Start server on port 80 (default for a web-browser, change to your requirements, e.g. 8080 if your Router uses port 80
                                // To access server from the outside of a WiFi network e.g. ESP8266WebServer server(8266) add a rule on your Router that forwards a
                                // connection request to http://your_network_ip_address:8266 to port 8266 and view your ESP server from anywhere.
@@ -56,29 +73,39 @@ long previousMillis = 0;        // will store last time Message was updated
 long interval = 30000;  
 
 void setup() {
+     delay(2000);
+  
   Serial.begin(115200); // initialize serial communications
-  WiFiManager wifiManager;
-  // New OOB ESP8266 has no Wi-Fi credentials so will connect and not need the next command to be uncommented and compiled in, a used one with incorrect credentials will
-  // so restart the ESP8266 and connect your PC to the wireless access point called 'ESP8266_AP' or whatever you call it below in ""
-  // wifiManager.resetSettings(); // Command to be included if needed, then connect to http://192.168.4.1/ and follow instructions to make the WiFi connection
-  // Set a timeout until configuration is turned off, useful to retry or go to sleep in n-seconds
-  wifiManager.setTimeout(180);
-  //fetches ssid and password and tries to connect, if connections succeeds it starts an access point with the name called "ESP8266_AP" and waits in a blocking loop for configuration
-  if(!wifiManager.autoConnect("SmartLedMessenger")) {
-    Serial.println(F("failed to connect and timeout occurred"));
-    delay(6000);
-    ESP.reset(); //reset and try again
-    delay(180000);
-  }
-  // At this stage the WiFi manager will have successfully connected to a network, or if not will try again in 180-seconds
-  Serial.println(F("WiFi connected..."));
-  //----------------------------------------------------------------------
-  server.begin(); Serial.println(F("Webserver started...")); // Start the webserver  configTime(0 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  message = "Smart Led Messenger (C) 2018";
   matrix.setIntensity(2);    // Use a value between 0 and 15 for brightness
   matrix.setRotation(0, 1);  // The first display is position upside down
   matrix.setRotation(1, 1);  // The first display is position upside down
   matrix.setRotation(2, 1);  // The first display is position upside down
   matrix.setRotation(3, 1);  // The first display is position upside down
+  display_message(message); // Display the message
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+     delay(500);
+     Serial.print("*");
+  }
+
+  Udp.begin(localPort);
+  setSyncProvider(getNtpTime);
+  
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  Serial.print("The IP Address of ESP8266 Module is: ");
+  Serial.print(WiFi.localIP());// Print the IP address
+  // At this stage the WiFi manager will have successfully connected to a network, or if not will try again in 180-seconds
+  Serial.println(F("WiFi connected..."));
+  display_message(WiFi.localIP().toString());
+  //----------------------------------------------------------------------
+  server.begin(); Serial.println(F("Webserver started...")); // Start the webserver  configTime(0 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
   server.on("/", GetMessage);
   wait    = 40;
   message = "Smart Led Messenger (C) 2018";
@@ -89,33 +116,69 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
-  display_message(message); // Display the message
-  unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis; 
-    GetServerMessage();
+  if (msgstatic) {
+    display_static_message(message);
+  } else {
+    display_message(message); // Display the message
   }
 }
 
-void display_message(String message){
-   for ( int i = 0 ; i < width * message.length() + matrix.width() - spacer; i++ ) {
+String padzero(int num) {
+  if (num < 10)
+    return '0'+String(num);
+  return String(num);
+}
+
+String parseCodes(String str) {
+  String remotemessage = String(str);
+  remotemessage.replace("{{time}}",  padzero(hour()) +":"+ padzero(minute()));
+  return remotemessage;
+}
+
+void display_static_message(String passedmsg) {
+  String remotemessage = parseCodes(passedmsg);
+  int letter = 0;
+  int x = 0;
+  int y = (matrix.height() - 8) / 2; // center the text vertically
+  while ( letter < remotemessage.length() ) {
+    matrix.drawChar(x, y, remotemessage[letter], HIGH, LOW, 1); // HIGH LOW means foreground ON, background OFF, reverse these to invert the display!
+    letter++;
+    x += width;
+  }
+  matrix.write(); // Send bitmap to display
+  delay(wait);
+  server.handleClient();
+  if (message != passedmsg) {
+    matrix.fillScreen(0);
+  }
+}
+
+void display_message(String passedmsg){
+  String remotemessage = parseCodes(passedmsg);
+  Serial.println(remotemessage);
+   for ( int i = 0 ; i < width * remotemessage.length() + matrix.width() - spacer; i++ ) {
     //matrix.fillScreen(LOW);
     int letter = i / width;
     int x = (matrix.width() - 1) - i % width;
     int y = (matrix.height() - 8) / 2; // center the text vertically
     while ( x + width - spacer >= 0 && letter >= 0 ) {
-      if ( letter < message.length() ) {        
-        matrix.drawChar(x, y, message[letter], HIGH, LOW, 1); // HIGH LOW means foreground ON, background OFF, reverse these to invert the display!
+      if ( letter < remotemessage.length() ) {        
+        matrix.drawChar(x, y, remotemessage[letter], HIGH, LOW, 1); // HIGH LOW means foreground ON, background OFF, reverse these to invert the display!
       }
       letter--;
       x -= width;
     }
     matrix.write(); // Send bitmap to display
     delay(wait/2);
+    server.handleClient(); // dans la boucle pour traitement rapide
+    if (message != passedmsg) {
+      matrix.fillScreen(0);
+      break;
+    }
   }
 }
 
+// not used for now
 void GetServerMessage() {  
     HTTPClient http;  //Declare an object of class HTTPClient 
     http.begin("URL_TO_GET_THE_STRING_TO_DISPLAY");  // http://www.smartledmessenger.com/slm.ashx?key=2 
@@ -126,12 +189,13 @@ void GetServerMessage() {
     }
     http.end(); 
 }
+
 void GetMessage() {
   webpage = ""; // don't delete this command, it ensures the server works reliably!
   append_page_header();
   String IPaddress = WiFi.localIP().toString();
   webpage += "<form action=\"http://"+IPaddress+"\" method='post'>";
-  webpage += "<P>Enter text for display: <INPUT type='text' size ='60' name='message' value='Enter message'></P></form><br>";
+  webpage += "<P>Enter text for display: <INPUT type='text' size ='60' name='message' value='"+message+"'></P></form><br>";
   append_page_footer();
   server.send(200, "text/html", webpage); // Send a response to the client to enter their inputs, if needed, Enter=defaults
   if (server.args() > 0 ) { // Arguments were received
@@ -139,6 +203,13 @@ void GetMessage() {
       String Argument_Name   = server.argName(i);
       String client_response = server.arg(i);
       if (Argument_Name == "message") message = client_response;
+      if (Argument_Name == "static") msgstatic = client_response == "1";
+      if (Argument_Name == "timezone") {
+        timeZone = client_response.toInt();
+        Serial.println(client_response);
+        setTime(getNtpTime());
+      }
+      if (Argument_Name == "wait") wait = client_response.toInt();
     }
   }
 }
@@ -168,4 +239,59 @@ void append_page_footer(){ // Saves repeating many lines of code for HTML page f
   webpage += F("<ul><li><a href='/'>Enter Message</a></li></ul>");
   webpage += "&copy; Smart Led Messenger";
   webpage += F("</body></html>");
+}
+
+
+
+//////////////////////////////////
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  sendNTPpacket(timeServer);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:                 
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
